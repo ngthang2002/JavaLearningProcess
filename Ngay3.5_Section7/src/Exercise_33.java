7ad3cd848a8a916294d3c9c092cede56aa885f58
ngthang2002 <ngthang200201@gmail.com>
2025-03-07 00:01:31 +0700

    Update Exercise 33

---
+++ Ngay3.5_Section7/src/Exercise_33.java
@@ -0,0 +1,55 @@
+public class Exercise_33 {
+    public static void main(String[] args) {
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+    }
+
+}
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt((point.getX() - this.x) * (point.getX() - this.x) + (point.getY() - this.y) * (point.getY() - this.y));
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+}
+
+
