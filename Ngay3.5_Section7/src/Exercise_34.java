84a3fafeb1235d7949beadd10de66994d098b6a2
ngthang2002 <ngthang200201@gmail.com>
2025-03-07 00:08:06 +0700

    Update Exercise 34

---
+++ Ngay3.5_Section7/src/Exercise_34.java
@@ -0,0 +1,54 @@
+public class Exercise_34 {
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+    }
+}
+
+class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
+
+class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost < 0 ? 0 : cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+
+class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
+
